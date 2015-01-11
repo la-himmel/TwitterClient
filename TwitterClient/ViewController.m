f3a3d0a55d1eb7ce5ce8beb959a3f5675dd6e2e1
Yekaterina <yekaterina.zhmud@gmail.com>
2015-01-11 20:45:32 +0300

    Cut bug

--- TwitterClient/ViewController.m
+++ TwitterClient/ViewController.m
@@ -16,8 +16,6 @@
 #define API_URL @"https://api.twitter.com/1.1/statuses/"
 #define TIMELINE @"user_timeline.json"
 #define HOME @"home_timeline.json"
-#define API_KEY @"nRpsj7pkldHieAbjQrHOdZCpb"
-#define CONSUMER_SECRET @"h3Ldr7GAVgsfnh9p15uwDMjRMSAfCB1OloU9quy8CyGeiQHnH9"
 
 #define NO_ACCOUNTS @"No accounts presented on device. Please go to Settings and add an account."
 
@@ -164,55 +162,6 @@
     if ([segue.identifier isEqualToString:@"embed_collection"]) {
         self.collectionVC = segue.destinationViewController;
     }
-}
-
-- (void)twitterConnection
-{
-    //1. URL encode the consumer key and the consumer secret according to RFC 1738.
-    NSString *encodedApiKey = [API_KEY stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSString *encodedSecret = [CONSUMER_SECRET stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    
-    //2. Concatenate the encoded consumer key, a colon character “:”, and the encoded consumer secret into a single string.
-    NSString *bearer = [NSString stringWithFormat:@"%@:%@", encodedApiKey, encodedSecret];
-    
-    //3. Base64 encode the string from the previous step.
-    NSString *base64EncodedString = [[bearer dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0];
-    
-    NSLog(@"%@", base64EncodedString);
-    
-    //send request
-    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
-    
-    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/oauth2/token"];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url
-                                                           cachePolicy:NSURLRequestUseProtocolCachePolicy
-                                                       timeoutInterval:60.0];
-    NSString *auth = [NSString stringWithFormat:@"Basic %@", base64EncodedString];
-    [request addValue:auth forHTTPHeaderField:@"Authorization"];
-    [request addValue:@"application/x-www-form-urlencoded;charset=UTF-8" forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPMethod:@"POST"];
-    NSData *postData = [@"grant_type=client_credentials" dataUsingEncoding:NSUTF8StringEncoding];
-    [request setHTTPBody:postData];
-
-    
-    NSURLSessionDataTask *postDataTask = [session dataTaskWithRequest:request
-                                                    completionHandler:^(NSData *data,
-                                                                        NSURLResponse *response,
-                                                                        NSError *error)
-    {
-        if (error) {
-            NSLog(@"Error: %@", [error description]);
-        }
-        if (data) {
-            NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data
-                                                                       options:NSJSONReadingMutableLeaves error:&error];
-            self.accessToken = [dictionary objectForKey:@"access_token"];
-            NSLog(@"access_token %@", self.accessToken);
-        }
-    }];
-    
-    [postDataTask resume];
 }
 
 #pragma mark - UIAlertView
